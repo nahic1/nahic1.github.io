f56706a38045261b4bb62f62d473e4c855b3ea99
nahic1 <65180712+nahic1@users.noreply.github.com>
2024-10-04 12:16:49 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,9 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-	    // Formulardaten
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-	    // Simple Validierung
-    if (name && email && message) {
-        document.getElementById('success-message').innerText = 'Danke, Ihre Nachricht wurde gesendet!';
-        document.getElementById('contact-form').reset(); // Formular zurücksetzen
-    } else {
-        document.getElementById('success-message').innerText = 'Bitte füllen Sie alle Felder aus.';
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector('header');
+    header.style.opacity = '0';
+    header.style.transition = 'opacity 1s';
+
+    setTimeout(() => {
+        header.style.opacity = '1';
+    }, 100);
 });

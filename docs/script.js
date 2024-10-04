e62c7b5f64efcc1b3968b7bbd143d994b90c1344
nahic1 <65180712+nahic1@users.noreply.github.com>
2024-10-04 12:26:42 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector('header');
-    header.style.opacity = '0';
-    header.style.transition = 'opacity 1s';
-
-    setTimeout(() => {
-        header.style.opacity = '1';
-    }, 100);
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); // Verhindert das Standard-Submit-Verhalten
+        alert("Danke für deine Nachricht!");
+    });
 });
